e55baf996e74fbda898f9e0ef88cd6a3cd16f42c
Elias <eduhoheli001@fh-kaernten.at>
2024-07-10 18:18:00 +0200

    added content

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
     id("kotlin-kapt")
-    id("com.guardsquare.appsweep") version "1.5.9"
+    id("com.guardsquare.appsweep") version "latest.release"
 }
 
 android {
